9a04dbac447c7cb2c76e1eb42b7476401f96d06e
parasop <63485824+parasop@users.noreply.github.com>
2020-11-29 21:29:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,36 @@ const canvas = require("discord-canvas");
 const welcomeCanvas = new canvas.Welcome();
 const a = "000000";
 //definite color
+//dm chat bot
+const Got = require("got");
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setActivity("PARAS GAMING");
+});
+
+//chat bot
+client.on("message", async message => {
+  if (message.author.bot) return;
+  //definite it so bot can't reply to another bot
+  if (message.channel.type === "dm") {
+    try {
+      const res = await Got(
+        `https://api.snowflakedev.xyz/chatbot?message=${Discord.Util.escapeMarkdown(
+          message.content
+        )}`
+      );
+      const json = await JSON.parae(res.body);
+
+      return;
+      message.channel.send(json.message);
+    } catch (error) {
+      //chat error
+      return;
+      message.channel
+        .send("API ERROR TRY AGAIN!👍🏻")
+        .then(() => console.log(error));
+    }
+  }
 });
 
 client.on("message", msg => {
@@ -41,9 +68,12 @@ client.on("guildMemberAdd", async member => {
     )
     .toAttachment();
 
-  let attachment = new Discord.MessageAttachment(image.toBuffer(), "welcome.png");//attachment  its requir buffer
+  let attachment = new Discord.MessageAttachment(
+    image.toBuffer(),
+    "welcome.png"
+  ); //attachment  its requir buffer
 
   client.channels.cache.get("769885454265876501").send(attachment);
-});//test it
+}); //test it
 
 client.login(process.env.TOKEN);
